2949908966c4f836daa75d2c91e7d054007794c3
croWbar005 <harshilsharma205@gmail.com>
2026-07-19 00:20:22 +0530

    calc.html basic structure

---
+++ script.js
@@ -0,0 +1,21 @@
+function sum(a,b){
+    let sum = a + b;
+    return sum;
+}
+
+function subtraction(a,b){
+    let diff = a - b;
+    return diff;
+}
+
+function multiply (a,b){
+    let product = a*b;
+    return product;
+}
+
+function division (a,b){
+    let result = a/b;
+    return result;
+}
+
+
